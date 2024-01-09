34820bdac532681110846fa2a171c32a5f3841a1
arigkubra <arigkubra@gmail.com>
2024-01-10 00:53:12 +0300

    navbar component added

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,11 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
-
+import Navbar from './components/Navbar'
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-        Hello world!
-      </h1>
+      <Navbar />
     </>
   )
 }
---
+++ src/components/Navbar.jsx
@@ -0,0 +1,7 @@
+export default function Navbar() {
+  return (
+    <div className="navbar bg-primary text-primary-content rounded-xl">
+      <a className="btn btn-ghost text-xl">daisyUI</a>
+    </div>
+  );
+}
